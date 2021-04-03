171412d056ee7a522fbb5116d7eef6d052b18eb2
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-04-03 11:30:25 -0500

    add info to admin page

--- server/routes/survey.router.js
+++ server/routes/survey.router.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+router.get('/', (req,res)=>{
+    let queryText=`SELECT * FROM "feedback" ORDER BY "id";`;
+    pool.query(queryText).then(result =>{
+        res.send(result.rows);
+    }).catch(error =>{
+        console.log('error getting surveys', error);
+        res.sendStatus(500);
+    })
+})
+
 router.post('/', (req,res)=> {
   let newSurvey=req.body
   let queryText=`INSERT INTO "feedback" ("feeling","understanding", "support", "comments") VALUES ($1, $2, $3, $4);`;  
--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -5,10 +5,47 @@ function Admin(){
     let [surveys, setSurveys]=useState([]);
 
     const getSurveys=()=>{
-        console.log('get surveys')
+        axios.get('/api/survey').then((response)=>{
+            setSurveys(response.data);
+        }).catch((err)=>{
+            alert('error', err);
+            console.log(err);
+        })
     }
+
+    useEffect(()=> {getSurveys()},[]);
     return (
         <>
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Feeling</th>
+                    <th>Understanding</th>
+                    <th>Support</th>
+                    <th>Comments</th>
+                    <th>flagged</th>
+                    <th>date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {surveys.map(survey =>{
+                    return(
+                        <>
+                        <tr>
+                            <td>{survey.id}</td>
+                            <td>{survey.feeling}</td>
+                            <td>{survey.understanding}</td>
+                            <td>{survey.support}</td>
+                            <td>{survey.comments}</td>
+                            <td>{survey.flagged}</td>
+                            <td>{survey.date}</td>
+                        </tr>
+                        </>
+                    )
+                })}
+            </tbody>
+        </table>
         </>
     )
 }
