dfe0162cc003099c139924f465aa7191783cc065
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-04-03 02:03:08 -0500

    displaying summary in review

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -2,6 +2,7 @@ import{ useSelector,useDispatch} from 'react-redux';
 import {useState} from 'react';
 import axios from 'axios';
 import {useHistory} from 'react-router-dom';
+import './Review.css'
 
 function Review(){
     const dispatch= useDispatch();
@@ -21,9 +22,20 @@ function Review(){
         return store.comments
     })
 
+    const submitSurvery=()=>{
+        console.log('in submitSurvery');
+    }
+
     return(
         <>
         <h2>Thank you for your input</h2>
+        <ul>
+            <li>Feelings: {feeling}</li>
+            <li>Understanding: {understanding}</li>
+            <li>Support: {support}</li>
+            <li>Comments: {comments}</li>
+        </ul>
+        <button onClick={submitSurvery}>Submit</button>
         </>
     )
 }
