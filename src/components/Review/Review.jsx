ffdc53e86c48cf9e1ec6bc7664634ba6f8a5d72f
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-04-03 01:57:54 -0500

    add store items to review component

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,7 +1,29 @@
+import{ useSelector,useDispatch} from 'react-redux';
+import {useState} from 'react';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
+
 function Review(){
+    const dispatch= useDispatch();
+    const history= useHistory();
+
+    const feeling= useSelector((store)=>{
+        return store.feeling;
+    })
+
+    const understanding= useSelector((store)=>{
+        return store.understanding;
+    })
+    const support= useSelector((store)=>{
+        return store.support;
+    })
+    const comments= useSelector((store)=>{
+        return store.comments
+    })
+
     return(
         <>
-        <h2>Review</h2>
+        <h2>Thank you for your input</h2>
         </>
     )
 }
